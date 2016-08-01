cfd8fbe2abd70743bc864680fe61ca25d62f193f
saulo <saulocn@gmail.com>
2016-08-01 09:01:55 -0300

    - ng-repeat;

--- alurapic/public/js/controllers/fotos-controller.js
+++ alurapic/public/js/controllers/fotos-controller.js
@@ -1,6 +1,14 @@
-angular.module('alurapic').controller('FotosController', function($scope){
-	$scope.foto = {
-		titulo: 'Leão',
-		url: 'http://www.fundosanimais.com/Minis/leoes.jpg'
-	};
+angular.module('alurapic').controller('FotosController', function($scope, $http){
+	$scope.fotos = [
+		{
+			titulo: 'Leão',
+			url: 'http://www.fundosanimais.com/Minis/leoes.jpg'
+		},	
+		{
+			titulo: 'Leão 2',
+			url: 'http://www.fundosanimais.com/Minis/leoes.jpg'
+		},
+	];
+
+	var promise = $http.get('v1/fotos');
 });
