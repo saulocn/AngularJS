4649c0f18a6558cc2fa982ec48582742ef77cc23
saulo <saulocn@gmail.com>
2016-08-01 09:07:48 -0300

    - "Atalho" com success e error;

--- alurapic/public/js/controllers/fotos-controller.js
+++ alurapic/public/js/controllers/fotos-controller.js
@@ -1,10 +1,12 @@
 angular.module('alurapic').controller('FotosController', function($scope, $http){
 	$scope.fotos = [];
 
-	var promise = $http.get('v1/fotos');
-	promise.then(function(retorno){
-		$scope.fotos = retorno.data;
-	}).catch(function(error){
-		console.log(error);
+	$http.get('v1/fotos')
+	.success(function(fotos){
+		$scope.fotos = fotos;
+	})
+	.error(function(erro){
+		console.log(erro);
 	});
+
 });
