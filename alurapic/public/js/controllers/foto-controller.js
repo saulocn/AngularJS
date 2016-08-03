9ff564b94d00bf25b470244cdf323fa78c3a7d50
saulo <saulocn@gmail.com>
2016-08-03 15:24:53 -0300

    - organizar lógicas reutilizáveis em serviços; - criar serviços que retornam promises;

--- alurapic/public/js/controllers/foto-controller.js
+++ alurapic/public/js/controllers/foto-controller.js
@@ -1,50 +1,35 @@
-angular.module('alurapic').controller('FotoController', function($scope, $http, $routeParams, recursoFoto){
+angular.module('alurapic').controller('FotoController', function($scope, $http, $routeParams, cadastroDeFotos, recursoFoto){
 	$scope.foto={};
 	$scope.mensagem = '';
 
 	
 
 	if($routeParams.fotoId){
-		$http.get('v1/fotos/'+$routeParams.fotoId)
-		.success(function(foto){
-			$scope.foto = foto;
-		})
-		.error(function(erro){
-			console.log(erro);
-			$scope.mensagem = "Não foi possível obter a foto de ID: "+$routeParams.fotoId;
-		});
+		recursoFoto.get({fotoId : $routeParams.fotoId}, 
+			function(foto){
+				$scope.foto = foto;
+			},
+			function(erro){
+				console.log(erro);
+				$scope.mensagem = "Não foi possível obter a foto de ID: "+$routeParams.fotoId;
+			});
 	}
 
 
 	$scope.submeter = function(){
 		console.log($scope.foto);
 		if($scope.formulario.$valid){
-			if($scope.foto._id){
-				recursoFoto.update({fotoId : $scope.foto._id}, $scope.foto,
-					function(){
-						console.log("Foto " + $scope.foto.titulo + " atualizada com sucesso!");
-						$scope.mensagem = "Foto " + $scope.foto.titulo + " atualizada com sucesso!";
-						$scope.formulario.$setPristine();
-					},
-					function(erro){
-						console.log(error);
-						$scope.mensagem = "Não foi possível atualizar a foto "+$scope.foto.titulo+"!";
-					});
-
-			} else {
-				console.log($scope.foto);
-				$http.post('v1/fotos', $scope.foto)
-				.success(function(){
-					console.log("Foto "+$scope.foto.titulo+" cadastrada com sucesso!");
-					$scope.mensagem = "Foto "+$scope.foto.titulo+" cadastrada com sucesso!";
+			cadastroDeFotos.cadastrar($scope.foto)
+			.then(function(dados){
+				$scope.mensagem = dados.mensagem;
+				if(resultado.inclusao){
 					$scope.foto = {};
-					$scope.formulario.$setPristine();
-				})
-				.error(function(error){
-					console.log(error);
-					$scope.mensagem = "Não foi possível cadastrar a foto "+$scope.foto.titulo+"!";
-				});
-			}
+				}
+				$scope.formulario.$setPristine();
+			})
+			.catch(function(erro){
+				console.log(erro.mensagem);
+			});
 		}
 	};
 });
--- alurapic/public/js/services/meus-servicos.js
+++ alurapic/public/js/services/meus-servicos.js
@@ -5,4 +5,41 @@ angular.module('meusServicos', ['ngResource'])
 			method : 'PUT'
 		}
 	});
+})
+.factory('cadastroDeFotos', function(recursoFoto, $q){
+	var servico = {};
+	servico.cadastrar = function(foto){
+		return $q(function(resolve, reject){
+			if(foto._id){
+				recursoFoto.update({fotoId : foto._id}, foto,
+					function(){
+						resolve({
+							mensagem: 'Foto '+foto.titulo+' atualizada com sucesso!',
+							inclusao: false
+						});
+					},
+					function(erro){
+						console.log(erro);
+						reject({
+							mensagem: 'Não foi possível alterar a foto: '+foto.titulo
+						});
+					});
+			} else {
+				recursoFoto.save(foto,
+					function(){
+						resolve({
+							mensagem: 'Foto '+foto.titulo+' cadastrada com sucesso!',
+							inclusao: true
+						});
+					},
+					function(erro){
+						console.log(erro);
+						reject({
+							mensagem: 'Não foi possível cadastrar a foto: '+foto.titulo
+						});
+					});
+			}
+		});
+	};
+	return servico;
 });
