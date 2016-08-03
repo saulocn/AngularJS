0920fcba50343b06fa4a2dcd9ac5debc79e93d4f
saulo <saulocn@gmail.com>
2016-08-03 09:46:36 -0300

    - a diretiva ng-click; - deleção de recurso com $http.delete; - o truque com a função splice para evitar uma nova requisição; - rotas do Angular com coringas (parametrizadas); - $routeParams e acesso a parâmetros; - alteração de recurso com $http.put; - único formulário para inclusão e alteração;

--- alurapic/public/js/controllers/foto-controller.js
+++ alurapic/public/js/controllers/foto-controller.js
@@ -1,20 +1,46 @@
-angular.module('alurapic').controller('FotoController', function($scope, $http){
+angular.module('alurapic').controller('FotoController', function($scope, $http, $routeParams){
 	$scope.foto={};
-	$scope.exibe = false;
 	$scope.mensagem = '';
+
+	if($routeParams.fotoId){
+		$http.get('v1/fotos/'+$routeParams.fotoId)
+		.success(function(foto){
+			$scope.foto = foto;
+		})
+		.error(function(erro){
+			console.log(erro);
+			$scope.mensagem = "Não foi possível obter a foto de ID: "+$routeParams.fotoId;
+		});
+	}
+
+
 	$scope.submeter = function(){
+		console.log($scope.foto);
 		if($scope.formulario.$valid){
-			console.log($scope.foto);
-			$http.post('v1/fotos', $scope.foto)
-			.success(function(){
-				$scope.foto = {};
-				console.log("Foto cadastrada com sucesso!");
-				$scope.mensagem = "Foto cadastrada com sucesso!";
-			})
-			.error(function(error){
-				console.log(error);
-				$scope.mensagem = "Não foi possível cadastrar a foto!";
-			});
+			if($scope.foto._id){
+				$http.put('v1/fotos/'+$scope.foto._id, $scope.foto)
+				.success(function(){
+					console.log("Foto " + $scope.foto.titulo + " atualizada com sucesso!");
+					$scope.mensagem = "Foto " + $scope.foto.titulo + " atualizada com sucesso!";
+				})
+				.error(function(error){
+					console.log(error);
+					$scope.mensagem = "Não foi possível atualizar a foto "+$scope.foto.titulo+"!";
+				});
+
+			} else {
+				console.log($scope.foto);
+				$http.post('v1/fotos', $scope.foto)
+				.success(function(){
+					console.log("Foto "+$scope.foto.titulo+" cadastrada com sucesso!");
+					$scope.mensagem = "Foto "+$scope.foto.titulo+" cadastrada com sucesso!";
+					$scope.foto = {};
+				})
+				.error(function(error){
+					console.log(error);
+					$scope.mensagem = "Não foi possível cadastrar a foto "+$scope.foto.titulo+"!";
+				});
+			}
 		}
 	};
 });
--- alurapic/public/js/controllers/fotos-controller.js
+++ alurapic/public/js/controllers/fotos-controller.js
@@ -1,6 +1,7 @@
 angular.module('alurapic').controller('FotosController', function($scope, $http){
 	$scope.fotos = [];
 	$scope.filtro = "";
+	$scope.mensagem = "";
 	
 	$http.get('v1/fotos')
 	.success(function(fotos){
@@ -10,4 +11,18 @@ angular.module('alurapic').controller('FotosController', function($scope, $http)
 		console.log(erro);
 	});
 
+	$scope.remover = function(foto){
+		$http.delete('v1/fotos/'+foto._id)
+		.success(function(){
+			var indiceFoto = $scope.fotos.indexOf(foto);
+			$scope.fotos.splice(indiceFoto,1);
+			console.log("Foto "+foto.titulo+" removida com sucesso!");
+			$scope.mensagem = "Foto "+foto.titulo+" removida com sucesso!";
+		})
+		.error(function(erro){
+			console.log(erro);
+			console.log("Não foi possível remover a foto: "+foto.titulo+"!");
+			$scope.mensagem = "Não foi possível remover a foto: "+foto.titulo+"!";
+		});
+	};
 });
